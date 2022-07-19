4cbea4a235128b4aa4680f0f05bf27bb3daaeac2
zindao <Raymi.Anselmo@gmail.com>
2022-07-19 13:10:42 +0200

    local bootstrap added in functions.php

--- functions.php
+++ functions.php
@@ -7,8 +7,9 @@ add_theme_support('title-tag');
 /* Load Styles */
 function load_styles() {
 
-    wp_register_style('styles-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), '5.1.3', 'all' );
+    wp_register_style('styles-bootstrap', get_template_directory_uri() . '/' . 'node_modules/bootstrap/dist/css/bootstrap.css', array(), '5.1.3', 'all' );
     wp_enqueue_style('styles-bootstrap'); 
+
 
     wp_register_style('styles-bundle', get_template_directory_uri() . '/dist/css/bundle.css', array(), '1.0', 'all' );
     wp_enqueue_style('styles-bundle');
